995970a76b45fe32a640a4c9172a8574f87a934f
kwonnahyun0125 <tns6186@naver.com>
2025-08-27 13:23:18 +0900

    chore: ignore and remove prisma/migrations from repo

--- prisma/migrations/20250628111122_/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `imageURL` on the `User` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "User" DROP COLUMN "imageURL",
-ADD COLUMN     "image" TEXT;
